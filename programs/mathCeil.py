96a5e3a5cfdcfbccc6de5166ef6a1b12495b1870
Suresh Mallela <80453962+sureshmallela9@users.noreply.github.com>
2026-03-03 05:45:30 +0530

    import math ex

---
+++ programs/mathCeil.py
@@ -0,0 +1,11 @@
+import math
+
+value = 22.2
+
+print(math.ceil(value))  #23
+
+value2 = 22.9
+
+print(math.ceil(value2)) #23
+
+# math.ceil method always return the smallest integer which greater than or equal to given number. it will round up the value.
---
+++ programs/mathFloor.py
@@ -0,0 +1,10 @@
+import math
+
+value = 3.14
+
+print(math.floor(value))  # 3
+
+value2 = 3.79
+print(math.floor(value2)) # 3
+
+#math.floor method always returns largest integer is less than or equal to the given number .
